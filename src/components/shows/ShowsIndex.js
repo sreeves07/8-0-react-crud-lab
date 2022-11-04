54560773f075867f0f80774bfafb1f5588f89952
sreeves07 <shamarreeves@pursuit.org>
2022-11-04 10:24:56 -0400

    search bar and delete

--- src/components/shows/ShowsIndex.js
+++ src/components/shows/ShowsIndex.js
@@ -9,14 +9,30 @@ import "./ShowsIndex.css";
 
 import { getAllShows } from "../../api/fetch";
 
+function filterShows(search,shows) {
+  return shows.filter((show) => {
+    return show.title.toLowerCase().match(search.toLowerCase())
+  })
+}
+
 export default function ShowsIndex() {
 
   const [loadingError, setLoadingError] = useState(false)
   const [shows, setShows] = useState([])
+  const [allShows, setAllShows] = useState([])
+  const [searchTitle, setSearchTitle] = useState("")
+
+  function handleTextChange(event) {
+    const title = event.target.value
+    const result = title.length ? filterShows(title, allShows) : allShows
+    setShows(result)
+    setSearchTitle(title)
+  }
 
   useEffect(() => {
     getAllShows()
       .then((res) => {
+        setAllShows(res)
         setShows(res)
         setLoadingError(false)
       })
@@ -41,9 +57,9 @@ export default function ShowsIndex() {
             Search Shows:
             <input
               type="text"
-              // value={searchTitle}
+              value={searchTitle}
               id="searchTitle"
-              // onChange={handleTextChange}
+              onChange={handleTextChange}
             />
           </label>
           <section className="shows-index">
